e644a498c50e815f532525725dcb5698954d39b6
Albert George <Albert.George@msadmindatummonitoring.onmicrosoft.com>
2018-03-15 09:47:00 +0530

    improved group association when adding a user

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -14,12 +14,19 @@ class UserController{
                 phone: user.phone
             }).then(newUser => {
                 if(user.groups && user.groups.length > 0){                
-                    var groupPromises = [];
+                    /*var groupPromises = [];
                     user.groups.forEach(group => {
                         groupPromises.push(newUser.addGroup(group.id));
                     });
                     Promise.all(groupPromises).then(() => {  
                         resolve(newUser);                  
+                    });*/
+                    var groupIds = [];
+                    user.groups.forEach(group => {
+                        groupIds.push(group.id);
+                    });
+                    newUser.addGroups(groupIds).then(() => {
+                        resolve(newUser);
                     });
                 }else{
                     this.db.Group.findAll({where: {isDefault: 1}}).then(group => {
